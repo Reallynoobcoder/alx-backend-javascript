c3ebf52f9adf065e8d6fadf672f78c5c2b137fe0
El imali hamza <elimal.hamza@gmail.com>
2024-06-04 19:43:03 +0000

    task 5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const readFileasync = require('./3-read_file_async');
+
+const app = http.Server((req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('This is the list of our students');
+
+    readFileasync(process.argv[2])
+      .then((students) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(`This is the list of our students:\n${students}`);
+      })
+      .catch((error) => {
+        res.statusCode = 500;
+        res.end(error.message);
+      });
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+const Port = 1245;
+
+app.listen(Port, 'localhost', () => {
+  console.log(`Server running at http://localhost:${Port}/`);
+});
+
+module.exports = app;
