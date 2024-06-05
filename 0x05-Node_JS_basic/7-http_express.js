340241421e56059e9d07b887b3a69604d2deb5a8
El imali hamza <elimal.hamza@gmail.com>
2024-06-05 13:20:31 +0000

    7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -13,7 +13,7 @@ app.get('/students', (req, res) => {
       res.send(`This is the list of our students\n${students}`);
     })
     .catch((error) => {
-      res.status(500).send(`Error: ${error.message}`);
+      res.status(500).send(`This is the list of our students\n${error.message}`);
     });
 });
 
