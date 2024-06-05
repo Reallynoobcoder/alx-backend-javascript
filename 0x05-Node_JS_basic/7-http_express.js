6ec08295ea8ddd13646819ac07405ee58ff9c569
El imali hamza <elimal.hamza@gmail.com>
2024-06-05 12:34:01 +0000

    7-http_express.js

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents('database.csv')
+    .then((students) => {
+      res.send(`This is the list of our students:\n${students}`);
+    })
+    .catch((error) => {
+      res.status(500).send(`Error: ${error.message}`);
+    });
+});
+
+const Port = 1245;
+
+app.listen(Port, () => {
+  console.log(`Server is running on port ${Port}`);
+});
+
+module.exports = app;
