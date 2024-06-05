dbac9d4a01f8bdbc948c29542390bd96be10be91
El imali hamza <elimal.hamza@gmail.com>
2024-06-05 11:28:44 +0000

    5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,41 +1,31 @@
 const http = require('http');
-const readFileasync = require('./3-read_file_async');
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
   if (req.url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    const originalConsoleLog = console.log;
-    console.log = () => {};
+    res.write('This is the list of our students\n');
 
-    readFileasync(process.argv[2])
-      .then((studentsData) => {
-        console.log = originalConsoleLog;
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(studentsData);
+    countStudents(process.argv[2])
+      .then((studentData) => {
+        res.end(studentData);
       })
       .catch((error) => {
-        console.log = originalConsoleLog;
-
         res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Cannot load the database');
+        res.end(error.message);
       });
   } else {
     res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
     res.end('Not Found');
   }
 });
 
-const Port = 1245;
-
-app.listen(Port, 'localhost', () => {
-  console.log(`Server running at http://localhost:${Port}/`);
+app.listen(1245, () => {
+  console.log('Server running at http://localhost:1245/');
 });
 
 module.exports = app;
